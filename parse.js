b2c5038bbb4917875b6c9d44adad53909ed59b59
Will Benyon <gitster@benyon.io>
2015-01-23 00:00:52 +0000

    Return next two trains for each station

--- parse.js
+++ parse.js
@@ -1,30 +1,37 @@
 var cheerio = require('cheerio');
 
 var parseHtml = function(data) {
+  var results = {};
+
   var $ = cheerio.load(data);
+  var resultsDom = $('ul.results');
 
-  var resultDom = $('ul.results li#result0');
+  for(result in ['0', '1']) {
+    var resultDom = $('li#result' + result, resultsDom);
 
-  var journeytime = $('strong:first-child', resultDom).text();
-  var journeyStartTime = journeytime.replace(/\s.*/, '');
+    var journeytime = $('strong:first-child', resultDom).text();
+    var journeyStartTime = journeytime.replace(/\s.*/, '');
 
-  var journeyDetails = $('small', resultDom);
-  var emTexts = [];
-  journeyDetails.find('em').each(function(_, e) {
-    emTexts.push($(e).text());
-  });
+    var journeyDetails = $('small', resultDom);
+    var emTexts = [];
+    journeyDetails.find('em').each(function(_, e) {
+      emTexts.push($(e).text());
+    });
 
-  var platformInfo = emTexts.filter(function(e) {
-    return /^Platform\n\t+\d+$/.test(e);
-  })[0];
+    var platformInfo = emTexts.filter(function(e) {
+      return /^Platform\n\t+\d+$/.test(e);
+    })[0];
 
-  var platform = '?';
-  if(platformInfo) {
-    platform = platformInfo.replace(/\w+\s+/, '');
+    var platform = '?';
+    if(platformInfo) {
+      platform = platformInfo.replace(/\w+\s+/, '');
+    }
+
+    results[result] = { 'time': journeyStartTime,
+                        'platform': platform };
   }
 
-  return { 'time': journeyStartTime,
-           'platform': platform };
+  return results;
 };
 
 module.exports = {
