54c6f31aecd352f89c735ca41b92647ae68d06e5
Will Benyon <gitster@benyon.io>
2015-01-23 09:08:14 +0000

    Fetch results for both Burgess Hill stations
    Now reports next train for both Burgess Hill and Wivelsfield station in
    results.

--- trains.js
+++ trains.js
@@ -1,25 +1,40 @@
+var async = require('async');
 var http = require('http');
 var parser = require('./parse.js');
 
-var stationResultsUrl = function() {
-  return process.env.TRAIN_LOOKUP_BASE_URL + '/VIC/BUG';
+var stationResultsUrl = function(destinationStation) {
+  return process.env.TRAIN_LOOKUP_BASE_URL + '/VIC/' + destinationStation;
 };
 
-var getTrains = function(callback) {
-  http.get(stationResultsUrl(), function(res) {
+var getTrainsForStation = function(station, trains, taskComplete) {
+  http.get(stationResultsUrl(station), function(res) {
     var data = [];
     res.setEncoding('utf8');
     res.on('data', function(d) {
       data.push(d);
     });
     res.on('end', function() {
-      var response =
-        { 'Burgess Hill' :
-          parser.parse(data.join('')) };
-      callback(response);
+      trains[station] = parser.parse(data.join(''));
+      taskComplete();
     });
   }).on('error', function(e) {
     console.log('Got error: ' + e.message);
+    taskComplete();
+  });
+};
+
+var getTrains = function(callback) {
+  var trains = {};
+  async.parallel([
+    function(taskCompleteCallback) {
+      getTrainsForStation('BUG', trains, taskCompleteCallback);
+    },
+    function(taskCompleteCallback) {
+      getTrainsForStation('WVF', trains, taskCompleteCallback);
+    }
+  ], function(err) {
+    if(err) return next(err);
+    callback(trains);
   });
 };
 
