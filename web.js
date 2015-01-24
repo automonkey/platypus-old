fd340a9463c008b8cc82030a126ca1aff1c26c24
Will Benyon <gitster@benyon.io>
2015-01-24 20:11:48 +0000

    Add Handlebars rendering of results

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var hbs = require('express3-handlebars');
 var trains = require('./lib/trains.js');
 
 app.use(logfmt.requestLogger());
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
   trains.getTrains(function(results) {
-    res.send(results);
+    res.render('trains', results);
   });
 });
 
