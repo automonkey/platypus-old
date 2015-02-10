c44a9cbf9bbef64f5f8332646cf2289b7baac675
Will Benyon <gitster@benyon.io>
2015-02-10 19:15:24 +0000

    Add font styling
    Quite possibly the most gratuitous use of Sass in history.

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express = require('express');
+var logfmt = require('logfmt');
+var path = require('path');
+var sassMiddleware = require('node-sass-middleware');
+
 var trains = require('./lib/trains.js');
 
+var app = express();
 app.use(logfmt.requestLogger());
+app.use(sassMiddleware( {
+  src: __dirname + '/sass',
+  dest: __dirname + '/public/stylesheets',
+  prefix:  '/stylesheets'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'jade');
 
@@ -36,5 +45,5 @@ app.get('/', function(req, res) {
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
